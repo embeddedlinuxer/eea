12d5c75689522af370237d33e62ca5853bea4d80
Daniel Koh <embeddedlinuxer@gmail.com>
2022-01-31 16:24:41 -0600

    workspace_eea without russian special

--- APP/SOURCE/ASM/api.asm
+++ APP/SOURCE/ASM/api.asm
@@ -1,6 +1,6 @@
 ;******************************************************************************
 ;* TMS320C3x/4x ANSI C Code Generator                            Version 5.11 *
-;* Date/Time created: Mon Jan 31 15:51:39 2022                                *
+;* Date/Time created: Mon Jan 31 16:14:26 2022                                *
 ;******************************************************************************
 	.regalias	; enable floating point register aliases
 fp	.set	ar3
@@ -17,7 +17,7 @@ FP	.set	ar3
 ;*   Calls              : Normal Library ASM calls                            *
 ;*   Debug Info         : Standard TI Debug Information                       *
 ;******************************************************************************
-;	C:\tic3x4x\c3x4x\cgtools\bin\ac30.exe -@_BOG.AAA 
+;	C:\tic3x4x\c3x4x\cgtools\bin\ac30.exe -@_BR8.AAA 
 	.file	"api.c"
 	.file	"Y:\workspace_eea\V615\APP\SOURCE\INCLUDE\GLBLS.H"
 	.file	"Y:\workspace_eea\V615\INCLUDE\stdarg.h"
